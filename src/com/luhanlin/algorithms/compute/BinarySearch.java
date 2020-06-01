f00273ea7b82adcdf15518b5d3f6c1a6b73a64bc
funnylu <allen_lu_hh@163.com>
2020-06-01 15:46:25 +0800

    调表以及二分查找

--- src/com/luhanlin/algorithms/compute/BinarySearch.java
+++ src/com/luhanlin/algorithms/compute/BinarySearch.java
@@ -1,7 +1,6 @@
 package com.luhanlin.algorithms.compute;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 /**
  * @description: 二分查找法
@@ -60,12 +59,92 @@ public class BinarySearch {
         return -1;
     }
 
+    /**
+     * 一组有序可重复的数组，找到第一个与value相等的元素下标
+     * @param a
+     * @param value
+     */
+    public static int getFirstValue(int[] a, int value){
+        int min = 0;
+        int max = a.length-1;
+
+        while (min <= max) {
+            int middle = min + ((max - min) >> 1); // 去中间值
+            if (a[middle] > value) {
+                max = middle -1;
+            } else if (a[middle] < value) {
+                min = middle +1;
+            } else{
+                if (middle == 0 || a[middle -1] != value) {
+                    return middle;
+                } else {
+                    max = middle -1;
+                }
+            }
+        }
+        return -1;
+    }
+    /**
+     * 一组有序可重复的数组，找到最后一个与value相等的元素下标
+     * @param a
+     * @param value
+     */
+    public static int getLastValue(int[] a, int value){
+        int min = 0;
+        int max = a.length-1;
+
+        while (min <= max) {
+            int middle = min + ((max - min) >> 1); // 去中间值
+            if (a[middle] > value) {
+                max = middle -1;
+            } else if (a[middle] < value) {
+                min = middle +1;
+            } else{
+                if (middle == a.length -1 || a[middle +1] != value) {
+                    return middle;
+                } else {
+                    min = middle + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 一组有序可重复的数组，找到第一个大于等于value的元素下标
+     * @param a
+     * @param value
+     */
+    public static int getFirstGtValue(int[] a, int value){
+        int min = 0;
+        int max = a.length-1;
+
+        while (min <= max) {
+            int middle = min + ((max - min) >> 1); // 去中间值
+            if (a[middle] >= value) {
+                if (middle == 0 || a[middle - 1] < value) {
+                    return middle;
+                } else {
+                    max = middle - 1;
+                }
+            } else{
+                min = middle + 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        int[] ints = {1, 2, 3, 4, 6, 5, 4, 33};
+        int[] a = {1, 2, 3, 4, 4, 4, 4, 6, 11, 13, 15};
 
-        Arrays.sort(ints);
+//        System.out.println(getFirstValue(a, 4));
+//        System.out.println(getLastValue(a, 4));
+        System.out.println(getFirstGtValue(a, 5));
 
-        System.out.println("recursionSearch: " + recursionSearch(ints, 4, 0, ints.length-1));
-        System.out.println("loopSearch: " + loopSearch(ints, 4));
+//        int[] ints = {1, 2, 3, 4, 6, 5, 4, 33};
+//        Arrays.sort(ints);
+//
+//        System.out.println("recursionSearch: " + recursionSearch(ints, 4, 0, ints.length-1));
+//        System.out.println("loopSearch: " + loopSearch(ints, 4));
     }
 }
